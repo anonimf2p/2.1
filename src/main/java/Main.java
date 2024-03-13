102be30a1e4f253d17ef32ff48794f8e64684dc9
anonimf2p <161606541+anonimf2p@users.noreply.github.com>
2024-03-13 13:26:11 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) {
+
+    String nazwa = "Hello world";
+    for(int i = 0; i < nazwa.length(); i++)  {
+      System.out.println(nazwa.charAt(i)); 
+    }
+  }
+
+}
